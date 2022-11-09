27b0160c8c061957b187d3bc73c9d7ba0d77c4b1
themarjanz <themarjanz@gmail.com>
2022-11-09 13:22:10 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        dolimatheme: {
+          primary: "#0FCFEC",
+          secondary: "#292E31",
+          accent: "#292E31",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+
+  plugins: [require("daisyui")],
+}
